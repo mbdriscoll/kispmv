fa3a920df2b6911901923f44aa85cabd42c3bb74
Michael Driscoll <mbdriscoll@gmail.com>
2013-11-08 21:44:12 -0800

    added examples

--- include/KISpMV.h
+++ include/KISpMV.h
@@ -14,10 +14,11 @@ template <typename elt_t> class CpuCooMatrix;
 // main matrix classes
 
 template <typename elt_t>
-class Vector : std::vector<elt_t> {
-    typedef std:;vector<elt_t> super;
+class Vector : public std::vector<elt_t> {
+    typedef std::vector<elt_t> super;
 
-    Vector(int nElems)
+  public:
+    Vector(int nElems, elt_t elt = elt_t())
       : super(nElems)
     { }
 };
@@ -28,99 +29,44 @@ class Matrix {
   protected:
 
     int m, n;
+    std::vector<int>   rowPtrs,
+                       colInds;
+    std::vector<elt_t> vals;
 
-    Matrix(int m, int n)
-      : m(m), n(n)
+    Matrix(int m, int n,
+        std::vector<int> &rowPtrs,
+        std::vector<int> &colInds,
+        std::vector<elt_t> &vals)
+      : m(m), n(n), rowPtrs(rowPtrs), colInds(colInds), vals(vals)
     { }
 
   public:
 
-    virtual int nnz() = 0;
-
-    // factory to create matrices
-    static Matrix<elt_t>* CreateFromCSR(int m, int n,
-        std::vector<int> &rowPtrs,
-        std::vector<int> &colInds,
-        std::vector<elt_t> &vals)
-    {
-        if (1) {
-            return CpuCsrMatrix<elt_t>::CreateFromCSR(m,n,rowPtrs,colInds,vals);;
-        } else {
-            return CpuCooMatrix<elt_t>::CreateFromCSR(m,n,rowPtrs,colInds,vals);;
+    template <typename x_elt_t>
+    Vector<x_elt_t> operator*(Vector<x_elt_t> x) {
+        Vector<x_elt_t> y(m, x_elt_t());
+        for (int i = 0; i < m; i++) {
+            for (int idx = rowPtrs[i]; idx < rowPtrs[i+1]; idx++) {
+                y[i] += vals[idx] * x[colInds[idx]];
+            }
         }
+        return y;
+    }
+
+    int nnz() {
+        return rowPtrs[m];
     }
 
     // factory to create matrices
-    static Matrix<elt_t>* CreateFromDense(int m, int n,
-        std::vector<int> &values)
+    static Matrix<elt_t> CreateFromCSR(int m, int n,
+        std::vector<int> &rowPtrsSrc,
+        std::vector<int> &colIndsSrc,
+        std::vector<elt_t> &valsSrc)
     {
-    }
-};
-
-// --------------------------------------------------------------------------
-// concrete matrix classes
-
-template <typename elt_t>
-class CpuCsrMatrix : Matrix<elt_t> {
-
-    typedef Matrix<elt_t> super;
-
-    std::vector<int>   &rowPtrs,
-                       &colInds;
-    std::vector<elt_t> &vals;
-
-    CpuCsrMatrix(int m, int n,
-        std::vector<int> &rowPtrs,
-        std::vector<int> &colInds,
-        std::vector<elt_t> &vals)
-      : super(m,n), rowPtrs(rowPtrs), colInds(colInds), vals(vals) {
-          std::cout << "Made CpuCsrMatrix" << std::endl;
-    }
-
-  public:
-
-    virtual int nnz() {
-        return rowPtrs[super::m];
-    }
-
-    static Matrix<elt_t>* CreateFromCSR(int m, int n,
-        std::vector<int> &rowPtrs,
-        std::vector<int> &colInds,
-        std::vector<elt_t> &vals)
-    {
-        return (Matrix<elt_t>*) new CpuCsrMatrix<elt_t>(m, n, rowPtrs, colInds, vals);
-    }
-};
-
-template <typename elt_t>
-class CpuCooMatrix : Matrix<elt_t> {
-
-    typedef Matrix<elt_t> super;
-
-    std::vector<int>   &rowInds,
-                       &colInds;
-    std::vector<elt_t> &vals;
-
-    CpuCooMatrix(int m, int n,
-        std::vector<int> &rowInds,
-        std::vector<int> &colInds,
-        std::vector<elt_t> &vals)
-      : super(m,n), rowInds(rowInds), colInds(colInds), vals(vals) {
-          std::cout << "Made CpuCooMatrix" << std::endl;
-    }
-
-  public:
-
-    virtual int nnz() {
-        return rowInds.size();
-    }
-
-    static Matrix<elt_t>* CreateFromCSR(int m, int n,
-        std::vector<int> &rowPtrs,
-        std::vector<int> &colInds,
-        std::vector<elt_t> &vals)
-    {
-        return (Matrix<elt_t>*) new CpuCooMatrix<elt_t>(m, n, rowPtrs, colInds, vals);
+        std::vector<int> *rowPtrs = new std::vector<int>(rowPtrsSrc);
+        std::vector<int> *colInds = new std::vector<int>(colIndsSrc);
+        std::vector<elt_t> *vals  = new std::vector<elt_t>(valsSrc);
+        return Matrix(m, n, *rowPtrs, *colInds, *vals);
     }
 };
 
